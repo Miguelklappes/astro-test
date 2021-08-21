b28423507c8d980b0349a163a49f931e2127ba0e
Miguelklappes <74425206+Miguelklappes@users.noreply.github.com>
2021-08-21 11:59:49 -0400

    Initial test

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="counter">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+  <div class="children">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(10);
+const add = () => (count.value = count.value + 1);
+const subtract = () => (count.value = count.value - 1);
+</script>
